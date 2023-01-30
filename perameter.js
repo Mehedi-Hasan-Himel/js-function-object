3b88b3aaf2081120b8da6ca6358492259ea4c1b4
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-30 23:47:13 +0600

    [ADD] perameter

--- perameter.js
+++ perameter.js
@@ -1,6 +1,23 @@
 function bringSingara(money) {
-   console.log(`Bring Singara`);
-   console.log(money);
+  console.log(`Money:`, money);
+  console.log(`Ai nen Singara`);
 }
 
-bringSingara(250);
+var taka = 300;
+// bringSingara(taka);
+
+
+function add(num1,num2) {
+   console.log(`Going to add :` , num1, num2);
+}
+
+// add(10,20);
+
+
+function sum(a,b,c,d,e) {
+   console.log(a, b, c, d, e);
+   var sum = a + b + c + d + e;
+   console.log(sum);
+}
+
+sum(10,20,30,40,50);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+function bringSingara(money) {
+  console.log(`Money:`, money);
+  console.log(`Ai nen Singara`);
+}
+
+bringSingara(100);
