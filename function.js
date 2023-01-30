884a33f4f0d362d2acd71115f059aac41d469a20
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-30 23:35:33 +0600

    [ADD] function declaration and call

---
+++ function.js
@@ -0,0 +1,9 @@
+// Function Declaration
+function startFan() {
+   console.log(   "Stadnup"   );
+   console.log(   "Walk towards the switch"   );
+   console.log(   "Press the switch"   );
+}
+
+// Calling the function
+startFan();
