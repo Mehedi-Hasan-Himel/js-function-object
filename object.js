3d0d9b1a93ae992c1a03e9ee0343ab8c03a706cd
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-31 07:19:30 +0600

    [ADD] object

---
+++ object.js
@@ -0,0 +1,23 @@
+var student = {
+   id: 115,
+   name: `Solaiman Khan`,
+   class: 9,
+   marks: 98,
+}
+
+var mobile = {
+   brand: `Samsung`,
+   price: 19000,
+   storage: `64GB`,
+   camera: `7MP`,
+}
+
+var myComputer = {
+   brand: `Lenovo`,
+   price: 39000,
+   color: `Silver`,
+   processor: `Intel Core i7`
+}
+
+myComputer.processor = `M1 Chip`
+console.log(myComputer);
