ff69ded94a8dbed241c68053a1dbd46665ee41c0
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-31 08:03:58 +0600

    [ADD] Switch

---
+++ switch.js
@@ -0,0 +1,19 @@
+const color = `blue`
+
+switch (color) {
+   case `red`:
+      console.log(`Color is red`);
+      break;
+   case `blue`:
+      console.log(`Color is blue`);
+      break;
+   case `green`:
+      console.log(`Color is green`);
+      break;
+   case `white`:
+      console.log(`Color is white`);
+      break;
+   default:
+      console.log(`Color is not red or blue or green`);
+      break;
+}
